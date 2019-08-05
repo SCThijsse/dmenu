24c1bf81ad08aba628bb5fcc3247cf1c3e634362
Sjoerd Thijsse <sjoerdthijsse@gmail.com>
2019-08-05 11:22:08 +0200

    fix: Replace terminus with roboto

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar    = 1;                   /* -b  option; if 0, dmenu appears at bottom        */
+static int centerbar = 0;                   /* -c  option; if 0, dmenu appears at bottom or top */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"xos4 Terminus:pixelsize=14:antialias=true:autohint=true"
+	"RobotoMono:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
